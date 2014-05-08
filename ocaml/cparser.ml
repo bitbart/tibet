b83c68213298add6cf58334c7e5ca31000ea8022
Sebastian Podda <sebastianpodda@gmail.com>
2014-05-08 16:26:02 +0200

    Added recursion with regexp - some bug fixes and input validations

--- ocaml/cparser.ml
+++ ocaml/cparser.ml
@@ -14,6 +14,16 @@
 #load "camlp4o.cma";;
 (* *)
 
+let printc c =
+	let s = Char.escaped c in 
+	if (Str.string_match (Str.regexp "[a-z]") s 0) then s 
+	else failwith "Invalid format for at least one action id or rec id  or clock id. Only a-z allowed.";;
+
+let printv c =
+	let s = Char.escaped c in 
+	if (Str.string_match (Str.regexp "[0-9]") s 0) then s 
+	else failwith "Invalid format for at least one clock value. Only integer values allowed.";;
+
 let rec sts p =
 	try
 		let c = Stream.next p in
@@ -26,13 +36,15 @@ let rec parse_resets =
 		parser
 		[< '',' ; x = parse_resets ?? "Missing a valid clock id after symbol ',' in at least one reset field" >] -> "\" />\n<reset id=\"" ^ x
 	| [< ''}' >] -> "\" />\n</resets>"
-	| [< 'x; y = parse_resets ?? "Missing '}' or ',' in at least one reset field" >] -> Char.escaped x ^ y
+	| [< 'x; y = parse_resets ?? "Missing '}' or ',' in at least one reset field" >] -> (printc x) ^ y
 and parse_guardValue =
 	parser
-	  [< '',' ; x = parse_guardType ?? "Missing a valid guard after symbol ',' in at least one guard field" >] -> "\" />\n<guard id=\"" ^ x
+	  [< '',' ; x = parse_guardType ?? "Missing a valid guard after symbol ',' in at least one guard field" >] -> 
+			if ((String.compare "\"" (String.sub x 0 1)) == 0) then failwith "A clock id cannot have an empty identifier!" 
+			else "\" />\n<guard id=\"" ^ x
 	| [< ''}' >] -> "\" />\n</guards>"	
 	| [< '';'; x = parse_resets ?? "Missing a valid clock id after symbol ';' in at least one reset field" >] -> "\" />\n</guards>\n<resets>\n<reset id=\"" ^ x
-	| [< 'x; y = parse_guardValue ?? "Missing '}' or ';' after a guard" >] -> Char.escaped x ^ y
+	| [< 'x; y = parse_guardValue ?? "Missing '}' or ';' after a guard" >] -> (printv x) ^ y
 and parse_guardType =
 	parser
 	  [< ''<' ; x = parse_guardValue  >] -> 
@@ -41,28 +53,39 @@ and parse_guardType =
 	| [< ''>' ; x = parse_guardValue >] -> 
 			if ((String.compare "\"" (String.sub x 0 1)) == 0) then failwith "Missing a valid clock value after symbol '>' in at least one guard field" 
 			else "\" op=\"great\" value=\"" ^ x
-	| [< 'x; y = parse_guardType ?? "Missing a valid guard in at least one guard field" >] -> Char.escaped x ^ y
+	| [< 'x; y = parse_guardType ?? "Missing a valid guard in at least one guard field" >] -> (printc x) ^ y
 and parse_guards' =
 	parser
 		[< '',' >] -> failwith "Missing a valid clock id after symbol ';' in at least one reset field"
 	| [< ''}' >] -> "\n</guards>\n<resets />"
-	| [< x = parse_resets >] -> "</guards>\n<resets>" ^ x
+	| [< x = parse_resets >] -> "</guards>\n<resets><reset id=\"" ^ x
 and parse_guards =
 	parser
 	  [< ''}' >] -> "\n</guards>\n<resets />"
 	| [< '';' ; x = parse_guards' ?? "Missing a valid reset in at least one reset field" >] -> x
 	| [< x = parse_guardType >] -> "\n<guard id=\"" ^ x 
+and parse_call =
+	parser
+    [< '']'; x = parse_contract' >] -> x 
+	| [< 'x; y = parse_call >] -> y
 and parse_contract' =
 	parser
-    [< ''?'; x = parse_contract' ?? "Missing a valid action name after symbol '?'" >] -> "\n<extaction id=\"" ^ x ^ "\n</extaction>"
-	| [< ''!'; x = parse_contract' ?? "Missing a valid action name after symbol '!'" >] -> "\n<intaction id=\"" ^ x ^ "\n</intaction>"
+    [< ''?'; x = parse_contract' ?? "Missing a valid action name after symbol '?'" >] -> 
+			if ((String.compare "\"" (String.sub x 0 1)) == 0) then failwith "An action cannot have an empty identifier!" 
+			else "\n<extaction id=\"" ^ x ^ "\n</extaction>"
+	| [< ''!'; x = parse_contract' ?? "Missing a valid action name after symbol '!'" >] -> 
+			if ((String.compare "\"" (String.sub x 0 1)) == 0) then failwith "An action cannot have an empty identifier!" 
+			else "\n<intaction id=\"" ^ x ^ "\n</intaction>"
 	| [< ''R'; ''E'; ''C'; ''['; x = parse_contract' ?? "Missing variable name in REC" >] -> "\n<rec name=\"" ^ x ^ "\n</rec>"
 	| [< '']'; x = parse_contract' ?? "Missing a valid action after REC[] operator" >] -> "\">" ^ x
-	| [< ''{'; g = parse_guards >] -> "\">\n<guards>" ^ g
-	| [< ''#'; x = parse_contract'; y = parse_contract' >] -> "\n<intchoice>" ^ x ^ y ^ "\n</intchoice>"
+	| [< ''['; x = parse_call >] -> x
+	| [< ''{'; g = parse_guards >] -> 			
+			if ((String.compare "\n<guard id=\"\"" (String.sub g 0 13)) == 0) then failwith "A clock id cannot have an empty identifier!" 
+			else "\">\n<guards>" ^ g
+	| [< ''#'; x = parse_contract'; y = parse_contract' >] -> "\n<intchoice>" ^ x ^ y ^ "\n</intchoice>" (* TODO: check for recursion as child - not allowed*)
 	| [< ''+'; x = parse_contract'; y = parse_contract' >] -> "\n<extchoice>" ^ x ^ y ^ "\n</extchoice>"
 	| [< ''.'; x = parse_contract'; y = parse_contract' >] -> "\n<sequence>" ^ x ^ y ^ "\n</sequence>"
-	| [< 'x; y = parse_contract' >] -> (Char.escaped x) ^ y
+	| [< 'x; y = parse_contract' >] -> (printc x) ^ y
 ;;
 
 (** REMOVE_SPACES **)
@@ -143,12 +166,20 @@ let rec remove_empties' l =
 
 let rec remove_empties s = remove_empties' (Str.split (Str.regexp "[\n]+") s);;
 
-let parse_contract c = remove_empties ("<contract>" ^ parse_contract' (Stream.of_string (infix_to_prefix c)) ^ "\n</contract>");;
+let preprocess_rec s = 
+	let s' = Str.global_replace (Str.regexp "\\(REC\\[[a-z]+\\]\\)\\(\\.\\)") "\\1(" s in
+	Str.global_replace (Str.regexp "\\(\\.\\)\\(\\[[a-z]+\\]\\)") ")\\2" s';;
+
+let postprocess_rec s = Str.global_replace (Str.regexp "\\(<rec name=\\\"\\)\\([a-z]+\\)\\(\\\">\\)\\([^\\&]+\\)\\(</rec>\\)") "\\1\\2\\3\\4<call name=\"\\2\" />\\5" s;;
+
+let parse_contract c = remove_empties ("<contract>" ^ parse_contract' (Stream.of_string (infix_to_prefix (preprocess_rec c))) ^ "\n</contract>");;
 
 let rec parse_multiple_contracts' l =
 	match l with
-	| c::l' -> (parse_contract c) ^ "\n" ^ parse_multiple_contracts' l' 
+	| c::l' -> (postprocess_rec (parse_contract c)) ^ "\n" ^ parse_multiple_contracts' l' 
 	| [] -> ""
 ;;
 
 let parse_multiple_contracts c = parse_multiple_contracts' (Str.split (Str.regexp "[|]+") c);;
+
+
