c931e447e0ed49482f0d02e9aaadb61bbb355ae3
Tiziana Cimoli <t.cimoli@unica.it>
2015-01-16 12:27:27 +0100

    Changed constructor names

--- ocaml/extTipi.ml
+++ ocaml/extTipi.ml
@@ -13,11 +13,11 @@ type extGuard =   SC of tsb_clock * tsb_ext_relation * int              (*simple
 
 type tsb_extGuard =  TSBExtGuard of extGuard;;
 
-type extTsb = Nil | Success |
-           IntChoice of (tsb_action * tsb_extGuard * tsb_reset * extTsb) list | 
-           ExtChoice of (tsb_action *tsb_extGuard * tsb_reset * extTsb) list  |
-           Rec of string * extTsb |
-           Call of string ;; 
+type extTsb = ExtNil | ExtSuccess |
+           ExtIntChoice of (tsb_action * tsb_extGuard * tsb_reset * extTsb) list | 
+           ExtExtChoice of (tsb_action *tsb_extGuard * tsb_reset * extTsb) list  |
+           ExtRec of string * extTsb |
+           ExtCall of string ;; 
 
 (*Examples*)
-let g = TSBExtGuard(And(Not(SC(TSBClock "x", Less, 4)),Or(Not(DC (TSBClock "x", TSBClock "t",  LessEq, 7)), DC(TSBClock "t", TSBClock "x", Eq, 5))));;
+(*let g = TSBExtGuard(And(Not(SC(TSBClock "x", Less, 4)),Or(Not(DC (TSBClock "x", TSBClock "t",  LessEq, 7)), DC(TSBClock "t", TSBClock "x", Eq, 5))));;*)
