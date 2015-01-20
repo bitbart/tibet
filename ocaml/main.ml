b632c05132bf22fee4eb35f1f2a9d94621cd2d24
Sebastian Podda <sebastianpodda@gmail.com>
2015-01-20 17:16:02 +0100

    Fixed error in main

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -86,7 +86,7 @@ let main =
 		| 5 -> 
       (
       match (Sys.argv.(1)) with
-      | "-delay" -> delay_net (float_of_string (Sys.argv.(2))) (Sys.argv.(2)) (Sys.argv.(3))
+      | "-delay" -> delay_net (float_of_string (Sys.argv.(2))) (Sys.argv.(3)) (Sys.argv.(4))
       | _ -> print_string ("Wrong input!\nUnrecognized or misused option: " ^ (Sys.argv.(1)) ^ "\n")
       )	
     | 8 -> 
