b81c1f01361678838e3ce4e7e1a3307ffc01fd33
LivioPompianu <LivioPompianu@localhost>
2015-01-31 21:44:24 +0100

    Improved Python testing (with assertion)

--- ocaml/tests/testPython.ml
+++ ocaml/tests/testPython.ml
@@ -6,8 +6,9 @@ open ExtTipi;;
 
 
 
-(** 						SECTION #1						**)
-(** THE FULL LIST OF GUARDS TO TEST.	**)
+(** 							SECTION #1						**)
+(** THE FULL LIST OF ELEMENTS TO TEST.	**)
+(* Guards. *)
 let guardList = 
 	[
 		(And(SC(TSBClock "x", ExtLess, 4),DC (TSBClock "x", TSBClock "t", ExtLessEq, 7)));
@@ -21,23 +22,74 @@ let guardList =
 		(And(Or(SC(TSBClock "t", ExtEq , 4),SC(TSBClock "x", ExtEq, 5)),  SC (TSBClock "s", ExtEq, 6)))
 	];;
 
-
+(* Clocks. *)
+let clockList = 
+	[
+		TSBClock "x";
+	];;
 
 
 
 (** 									SECTION #2									**)
 (** TEST: PAST, INVRESET, SUBTRACT, EQUIVALENCE.	**)
-(* Test a single element with a single function. *)
-let test' functionTest guard =
-	try let x = (functionTest guard) in (ignore x; true) with _ -> false;;
+(* Test a single element with a given function. *)
+let testElement functionTest element =
+	try let x = (functionTest element) in (ignore x; true) with _ -> false;;
 
-(* Test multiple elements with a single function. *)
-let rec test functionTest guardList  = 
-	match guardList with
+(* Test a list of elements with a given function. *)
+let rec testElementList functionTest elementList  = 
+	match elementList with
 	| [] -> true
-	| h::t -> (test' functionTest h) && (test functionTest t);;
+	| h::t -> (testElement functionTest h) && (testElementList functionTest t);;
 
-(* Testing past. *)
+(* Test a couple of elements with a given function. *)
+let testCouple functionTest element1 element2 =
+	try let x = (functionTest element1 element2) in (ignore x; true) with _ -> false;;
+
+(* Test a list of couples. *)
+let rec testCoupleList functionTest coupleList  = 
+	match coupleList with
+	| [] -> true
+	| (h1, h2)::t -> (testCouple functionTest h1 h2) && (testCoupleList functionTest t);;
+
+(* Given a list of element, returns all the possible couples. *)
+let makeCouples elementList =
+	(	
+	let rec makeCouples' elementList originalList =
+		(
+			match elementList with
+				| [] -> []
+				| h::t -> 
+					(
+					let rec makeCouples'' element list = 
+						match list with
+							| [] -> []
+							| (h'::t') -> (element, h')::(makeCouples'' element t')
+					in makeCouples'' h originalList
+					)@(makeCouples' t originalList)
+		) in makeCouples' elementList elementList
+	);;
+
+(* Testing Past. *)
 let testPast =
-	let x = assert ((test past guardList) == true) in
-	ignore x; "Done";
+	let x = assert ((testElementList past guardList) == true) in
+	ignore x; "Done";;
+
+(* Testing InvReset. *)
+let testInvReset = "ToDO";;
+
+(* Testing Subtract. *)
+let testSubtract =
+	let x = assert ((testCoupleList subtract (makeCouples guardList)) == true) in
+	ignore x; "Done";;
+
+(* Testing Equivalence. *)
+let testEquivalence =
+	let x = assert ((testCoupleList equivalence (makeCouples guardList)) == true) in
+	ignore x; "Done";;
+
+(* Main test function that launches all tests. *)
+let testPython =
+	let x = testPast in ignore x; 
+	let x = testSubtract; in ignore x;
+	let x = testEquivalence in ignore x; "Done";;
